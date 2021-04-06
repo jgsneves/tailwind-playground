e3088900912160637ced5bf1f46f914b8dbedff1
joao gabriel neves <jgsn.adv@gmail.com>
2021-04-06 11:20:29 -0300

    goingoin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hero-image': "url(../static/media/home-back.png)"
+      })
+    },
+    fontFamily: {
+      'h1': ['Sofia Pro Black'],
+      'h2': ['Sofia Pro Black'],
+    }
   },
   variants: {},
   plugins: [],
